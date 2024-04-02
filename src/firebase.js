7886ac4fae8d6a397271f8c5e95f47a13dbe9b09
pooh <6442086926@student.chula.ac.th>
2024-04-02 23:01:25 +0700

    Add firebase.js for firebase db

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAnw1wDqcBXRQqxUDzes16_pzVbVIZqlhE",
+    authDomain: "web-prog-286ec.firebaseapp.com",
+    projectId: "web-prog-286ec",
+    storageBucket: "web-prog-286ec.appspot.com",
+    messagingSenderId: "442024938878",
+    appId: "1:442024938878:web:627e90f5a402ac39efb72f",
+    measurementId: "G-QBRZCHLSC6"
+  };
